fd46e7fba4ba32f26186d04a12c88c545987a699
potapov-eo <potapov.eo@yandex.ru>
2021-01-18 01:43:02 +0300

    28

--- Tasks/6.FindTheOddInt/FindTheOddInt.js
+++ Tasks/6.FindTheOddInt/FindTheOddInt.js
@@ -5,6 +5,7 @@
     Всегда будет только одно целое число, которое встречается нечетное количество раз.*/
 
 function findOdd(arr) {
+
 let res=arr[0]
    arr.forEach((num)=>{
      if ( arr.filter((n)=>n===num).length % 2 !== 0 ){
@@ -14,4 +15,5 @@ let res=arr[0]
    return res
 }
 console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+console.log(a)
 
