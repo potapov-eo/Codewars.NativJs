4d5a494249e411b169413469545b5685adcfaebc
potapov-eo <potapov.eo@yandex.ru>
2021-03-03 23:15:42 +0300

    WhoLikesIt

---
+++ Tasks/6.WhoLikesIt/WhoLikesIt.js
@@ -0,0 +1,29 @@
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+*/
+
+function likes(names) {
+    if (names.length === 0) {
+        return `no one likes this`
+    }
+    if (names.length === 1) {
+        return `${names[0]} likes this`
+    }
+    if (names.length === 2) {
+        return `${names[0]} and ${names[1]} like this`
+    }
+    if (names.length === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    }
+    if (names.length > 3) {
+        return `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+    }
+}
+
+
+console.log(likes(["Peter"]) === "Peter likes this")
+console.log(likes(["Jacob", "Alex"]) === "Jacob and Alex like this")
+console.log(likes(["Max", "John", "Mark"]) === "Max, John and Mark like this")
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]) === "Alex, Jacob and 2 others like this")
